6191be3508dcb22dfa89bb21c3d1cd5fe5fff614
riley <rileyfarro@gmail.com>
2022-04-30 23:51:08 +0800

    - Created CSVParser interface to abstract its function when you want to change your OpenCSV library to something else. - Created CompanyListingsParser for the implementation of CSVParser. - Finished StockRepositoryImpl and strictly follow the single source of truth pattern.

---
+++ app/src/main/java/com/rpfcoding/stockmarketwithopencsv/data/csv/CSVParser.kt
@@ -0,0 +1,8 @@
+package com.rpfcoding.stockmarketwithopencsv.data.csv
+
+import java.io.InputStream
+
+interface CSVParser<T> {
+
+    suspend fun parse(stream: InputStream): List<T>
+}
--- app/src/main/java/com/rpfcoding/stockmarketwithopencsv/data/repository/StockRepositoryImpl.kt
+++ app/src/main/java/com/rpfcoding/stockmarketwithopencsv/data/repository/StockRepositoryImpl.kt
@@ -1,7 +1,10 @@
 package com.rpfcoding.stockmarketwithopencsv.data.repository
 
+import com.rpfcoding.stockmarketwithopencsv.data.csv.CSVParser
+import com.rpfcoding.stockmarketwithopencsv.data.csv.CompanyListingsParser
 import com.rpfcoding.stockmarketwithopencsv.data.local.StockDatabase
 import com.rpfcoding.stockmarketwithopencsv.data.mapper.toCompanyListing
+import com.rpfcoding.stockmarketwithopencsv.data.mapper.toCompanyListingEntity
 import com.rpfcoding.stockmarketwithopencsv.data.remote.StockApi
 import com.rpfcoding.stockmarketwithopencsv.domain.model.CompanyListing
 import com.rpfcoding.stockmarketwithopencsv.domain.repository.StockRepository
@@ -16,7 +19,8 @@ import javax.inject.Singleton
 @Singleton
 class StockRepositoryImpl @Inject constructor(
     private val api: StockApi,
-    private val db: StockDatabase
+    private val db: StockDatabase,
+    private val parser: CSVParser<CompanyListing>
 ) : StockRepository {
 
     private val dao = db.dao
@@ -43,12 +47,23 @@ class StockRepositoryImpl @Inject constructor(
             val remoteListings = try {
                 val response = api.getListings()
 
+                parser.parse(response.byteStream())
             } catch (e: IOException) {
                 e.printStackTrace()
                 emit(Resource.Error(message = "Could not load data"))
+                null
             } catch (e: HttpException) {
                 e.printStackTrace()
                 emit(Resource.Error(message = "Could not load data"))
+                null
+            }
+
+            remoteListings?.let { listings ->
+                dao.clearCompanyListings()
+                dao.insertCompanyListings(listings.map { it.toCompanyListingEntity() })
+
+                emit(Resource.Success(data = dao.searchCompanyListing("").map { it.toCompanyListing() }))
+                emit(Resource.Loading(isLoading = false))
             }
         }
     }
